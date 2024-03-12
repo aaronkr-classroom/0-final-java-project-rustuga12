5f3436b4257519b42ad236b9a985c618b32e5940
rustuga12 <wotjd7662@naver.com>
2024-03-12 14:39:37 +0900

    Level1 끝!

---
+++ src/BookMarket.java
@@ -0,0 +1,12 @@
+
+public class BookMarket {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to shopping Mall!");
+		System.out.println("welcome to BookMarket!!");
+		
+
+	}
+
+}
