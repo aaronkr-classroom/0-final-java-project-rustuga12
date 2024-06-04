b6779fdfe3e13ad291b10d48f3d171b5b2e81b7a
rustuga12 <wotjd7662@naver.com>
2024-06-04 16:00:19 +0900

    임시저장

---
+++ src/com/market/main/welcome.java
@@ -0,0 +1,11 @@
+package com.market.main;
+
+import                                              
+public class welcome {
+
+	public static void main(String[] args) {
+		new GuestWindow("고객 정보입력", 0, 0, 1000, 750);
+
+	}
+
+}
